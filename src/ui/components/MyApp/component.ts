4cc135a3d105ef4d64c8f45d9103c96c10a7d8c6
Harsh Rohila <harsh_rohila@trimble.com>
2019-11-02 08:09:50 +0530

    glimmer component which adds message in a message list

--- src/ui/components/MyApp/component.ts
+++ src/ui/components/MyApp/component.ts
@@ -1,5 +1,22 @@
-import Component from '@glimmer/component';
+import Component, { tracked } from '@glimmer/component';
 
 export default class MyApp extends Component {
+    @tracked messages = [];
 
+    didInsertElement() {
+        this.element.addEventListener('got-messages', this.gotMessages.bind(this), false);
+    }
+    
+    gotMessages(event: CustomEvent) {
+        this.messages = event.detail;
+    }
+
+    addMessage() {
+        const message = this.element.querySelector('input').value;
+        if (message) {
+            const messagesCopy = this.messages.slice();
+            messagesCopy.push(message);
+            this.messages = messagesCopy;
+        }
+    }
 }
